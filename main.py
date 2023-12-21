d19b191aeb3e86b53fb9b0564f151df128cb8bb0
DO_S <rlaxogus7094@naver.com>
2023-12-21 11:15:29 +0900

    최종 업데이트

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-import hashlib
-
 from flask import Flask, render_template, request, make_response
 import json
 import hashlib
 import os
 from config import *
+from collections import OrderedDict
 
 app = Flask(__name__)
 
 
+def allname() -> str:
+    name_all = str()
+    for person in 사람들:
+        name_all.join(person)
+
+    return name_all
+
+
 @app.route('/', methods=["GET", "POST"])
 def vote():
-    name_hash = hashlib.md5((첫번째사람 + 두번째사람).encode()).hexdigest()
+    name_hash = hashlib.md5(allname().encode()).hexdigest()
+
     if request.cookies.get(name_hash) == '1':
         return render_template('message.html', icon='error', message='이미 투표하셨습니다.')
 
     if request.method == "GET":
-        return render_template('index.html', first=첫번째사람, last=두번째사람)
+        return render_template('index.html', people=사람들)
 
     elif request.method == "POST":
         voted = request.form['vote']
@@ -29,7 +37,8 @@ def vote():
             json.dump(read_data, f, ensure_ascii=False, indent='\t')
 
         resp = make_response(render_template('message.html', icon='success', message='투표 완료'))
-        name_hash = hashlib.md5((첫번째사람+두번째사람).encode()).hexdigest()
+
+        name_hash = hashlib.md5(allname().encode()).hexdigest()
         resp.set_cookie(name_hash, '1')
 
         return resp
@@ -39,24 +48,28 @@ def vote():
 def result_check():
     with open('result.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
+        dict_data = dict(json_data)
 
-    first_count, second_count = json_data.get(첫번째사람), json_data.get(두번째사람)
+    participants_count = int()
 
-    participants_count = first_count + second_count
+    for key, items in dict_data.items():
+        participants_count += items
 
-    if first_count > second_count:
-        winner = f'"{첫번째사람}" 우승'
-    elif first_count == second_count:
-        winner = '무승부'
-    else:
-        winner = f'"{두번째사람}" 우승'
+    # 맥스값 찾아서 우승 띄우게
+    max_member = max(사람들)  # 사람 이름으로 나옴
+    winner = f"{max_member} 우승"
 
-    return render_template('result.html', result=json_data, count=participants_count, winner=winner)
+    return render_template('result.html', result=json_data, count=participants_count, winner=winner,
+                           len=len, people=사람들)
 
 
 if __name__ == '__main__':
     if not os.path.isfile('result.json'):
         with open('result.json', 'w', encoding='utf-8') as f:
-            json.dump({첫번째사람: 0, 두번째사람: 0}, f, ensure_ascii=False, indent='\t')
+            dump_data = OrderedDict()
+            for person in 사람들:
+                dump_data[person] = 0
+
+            json.dump(dump_data, f, ensure_ascii=False, indent='\t')
 
     app.run(host='0.0.0.0', port=80)
